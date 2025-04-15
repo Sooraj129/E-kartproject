600f1edcf8c10c87357113bd2c9da31978c83b4f
Soorajss1 <soorajcrazy2001@gmail.com>
2025-04-15 09:25:25 +0530

    changes in the packages

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest, syncDeleteUser, syncUserCreation, syncUserUpdate } from "@/Config/inngest";
 
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [
-    syncUserCreation,
-    syncUserUpdate,
-    syncDeleteUser
-  ],
-});
+export const { GET, POST, PUT } = serve(
+  inngest,
+  [syncUserCreation, syncUserUpdate, syncDeleteUser]
+);
+
